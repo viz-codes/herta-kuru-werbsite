68d2c188363ff3ec121565dc904aa0b66c9b2442
vizual.s <79601265+viz-codes@users.noreply.github.com>
2023-06-04 17:55:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+mp3 = ["mp3/kuru1.mp3", "mp3/kuru2.mp3", /*"mp3/kuru3.mp3"*/]
+
 function squishkuru() {
     var container = document.getElementById("images");
     var gif = document.createElement("img");
+    var sound = document.createElement("audio");
+    var chosen = mp3[mp3.length * Math.random() | 0];
+    sound.src = chosen;
+    sound.autoplay = true;
+    sound.className = "sound";
     gif.src = "herta.gif";
     gif.className = "animated";
-    //gif.style.zIndex = container.querySelectorAll("img").length + 1;
     container.appendChild(gif);
+    container.appendChild(sound);
 
     setTimeout(function() {
         container.removeChild(gif);
-    }, 2000);
+        container.removeChild(sound);
+    }, 1500);
 }
